3dc0e4da70d051a2168ca2a31513da20a82c07bd
Rafael Melo <126472498+PorousBunion8@users.noreply.github.com>
2026-03-20 20:24:15 -0300

    Refactor btree structure and update function signatures

--- arvoreb.h
+++ arvoreb.h
@@ -3,19 +3,23 @@
 
 #include <stdbool.h>
 
-#define Tam 5
+#define TAM 5
+
+typedef struct Registro{
+    int matricula;
+    long byte_offset;
+}Registro;
 
 typedef struct btree{
-    int keys[2*Tam-1];
-    struct btree *children[2*Tam];
+    Registro dados[TAM - 1];
+    struct btree *children[TAM];
     int n;
     bool leaf;
 } btree;
 
-btree* createnode(bool leaf);
-btree* searchnode(btree* node, int key);
-void insert(btree* node, int key);
-void insertnonfull(btree* node, int key);
-void splitchild(btree* parent, int i, btree* child);
+btree* create_node(bool leaf);
+void insert(btree** root, Registro reg);
+void free_tree(btree* node);
+Registro* search_node(btree* node, int matricula);
 
 #endif
